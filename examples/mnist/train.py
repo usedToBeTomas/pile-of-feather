9540e21eeaec4d2c9b482595a727d513f2ed0979
usedToBeTomas <85880657+usedToBeTomas@users.noreply.github.com>
2023-12-07 11:58:53 +0100

    Update

--- examples/mnist/train.py
+++ examples/mnist/train.py
@@ -4,8 +4,8 @@ from pileoffeather import nn, data_loader
 model = nn.create(name = "mnist", layers = [[784, "input"], [144, 'relu'], [10, 'sigmoid']])
 
 #Upload mnist dataset
-input_dataset = data_loader.load(data_type = "gz", path = "train-images-idx3-ubyte.gz", start_index = 16, input_number = 784, divide = 255)
-output_dataset = data_loader.load(data_type = "gz", path = "train-labels-idx1-ubyte.gz", start_index = 8, one_hot = 10)
+X = data_loader.load(data_type = "gz", path = "train-images-idx3-ubyte.gz", start_index = 16, input_number = 784, divide = 255)
+Y = data_loader.load(data_type = "gz", path = "train-labels-idx1-ubyte.gz", start_index = 8, one_hot = 10)
 
 #Train the neural network using backpropagation
-nn.mbgd(model, input_dataset, output_dataset, batch_size = 16, epoch_number = 2, rate = 1)
+nn.mbgd(model, X, Y, batch_size = 16, epoch_number = 2, rate = 1)
